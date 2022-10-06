7169b98a3690064786597fdbcec0463458cc14f1
RAM KUMAR N <103347695+ra-mkum-ar@users.noreply.github.com>
2022-10-06 20:45:18 +0530

    Add files via upload

---
+++ ARITHMETIC OPERATORS.py
@@ -0,0 +1,26 @@
+'''   +  ADDITION
+      -  SUBTRACTION
+      *  MULTIPLICATION
+      /  DIVIDE (FLOAT)
+      %  MODULUS
+      // FLOOR DIVISION
+      ** EXPONENT
+      
+      '''
+
+a=10
+b=5
+c=a+b
+print("c=",c)
+c=a-b
+print("c=",c)
+c=a*b
+print("c=",c)
+c=a/b
+print("c=",c)
+c=a%b
+print("c=",c)
+c=a//b
+print("c=",c)
+c=a**b
+print("c=",c)
